213090ccdfdcabc03db27edd151443f8ffc6dbe2
Sreedhar Reddy Kikkuru <kikkurusreedhar@gmail.com>
2022-03-19 14:53:11 +0000

    Full feature/leave chnaes available. Completed lead changes.
    done.

---
+++ core/lead.py
@@ -0,0 +1,16 @@
+##
+# Lead feature functionalities.
+##
+
+
+#Create Lead.
+def create_lead():
+  print "Created Lead successfully."
+
+#Update Lead.
+def udpate_lead():
+  print "Updated Lead successfully."
+
+#Update Lead.
+def delete_lead():
+  print "Deleted Lead successfully."
